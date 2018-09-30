af8dd990de8dabd8b1f0521b69fa662397b04a96
imannie <tranntphuong@gmail.com>
2018-09-30 16:31:44 -0700

    fixed name on bash branch

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-cat templates/top.html content/mid_index.html templates/bottom.html > docs/index.html 
-cat templates/top.html content/mid_about.html templates/bottom.html > docs/about.html 
-cat templates/top.html content/mid_work.html templates/bottom.html > docs/work.html 
-cat templates/top.html content/mid_contact.html templates/bottom.html > docs/contact.html 
+cat templates/top.html content/index.html templates/bottom.html > docs/index.html
+cat templates/top.html content/about.html templates/bottom.html > docs/about.html
+cat templates/top.html content/work.html templates/bottom.html > docs/work.html
+cat templates/top.html content/contact.html templates/bottom.html > docs/contact.html
